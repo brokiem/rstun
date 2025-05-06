f9f96111e4ed4254546efe17994e6603e87708fc
brokiem <milyhqvideo@gmail.com>
2025-05-06 08:52:24 +0800

    Use aes-128-gcm as cipher suite

--- src/lib.rs
+++ src/lib.rs
@@ -40,8 +40,8 @@ lazy_static! {
 type PooledBuffer = Block<'static, Vec<u8>>;
 
 pub const SUPPORTED_CIPHER_SUITE_STRS: &[&str] = &[
-    "chacha20-poly1305",
-    "aes-256-gcm",
+    // "chacha20-poly1305",
+    // "aes-256-gcm",
     "aes-128-gcm",
     // the following ciphers don't work at the moement, will look into it later
     // "ecdhe-ecdsa-aes256-gcm",
@@ -53,8 +53,8 @@ pub const SUPPORTED_CIPHER_SUITE_STRS: &[&str] = &[
 ];
 
 pub static SUPPORTED_CIPHER_SUITES: &[rustls::SupportedCipherSuite] = &[
-    cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
-    cipher_suite::TLS13_AES_256_GCM_SHA384,
+    // cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
+    // cipher_suite::TLS13_AES_256_GCM_SHA384,
     cipher_suite::TLS13_AES_128_GCM_SHA256,
 ];
 
@@ -65,10 +65,10 @@ impl std::str::FromStr for SelectedCipherSuite {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "chacha20-poly1305" => Ok(SelectedCipherSuite(
-                cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
-            )),
-            "aes-256-gcm" => Ok(SelectedCipherSuite(cipher_suite::TLS13_AES_256_GCM_SHA384)),
+            // "chacha20-poly1305" => Ok(SelectedCipherSuite(
+            //     cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
+            // )),
+            // "aes-256-gcm" => Ok(SelectedCipherSuite(cipher_suite::TLS13_AES_256_GCM_SHA384)),
             "aes-128-gcm" => Ok(SelectedCipherSuite(cipher_suite::TLS13_AES_128_GCM_SHA256)),
             // "ecdhe-ecdsa-aes256-gcm" => Ok(SelectedCipherSuite(
             //     rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
@@ -89,7 +89,7 @@ impl std::str::FromStr for SelectedCipherSuite {
             //     rustls::cipher_suite::TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
             // )),
             _ => Ok(SelectedCipherSuite(
-                cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
+                cipher_suite::TLS13_AES_128_GCM_SHA256,
             )),
         }
     }
